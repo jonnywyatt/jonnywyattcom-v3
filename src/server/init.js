2a98d6ba34a0bd94c404f187145b40d0288611cf
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2017-01-11 21:34:47 +0000

    Lint

--- src/server/init.js
+++ src/server/init.js
@@ -31,7 +31,7 @@ app.get('/api/articles', apiArticle.getAll);
 app.post('/api/articles', apiArticle.post);
 
 app.get('*', reactInit(appConfig).init);
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   logger.info('Node app is running on port', app.get('port'));
 });
 
