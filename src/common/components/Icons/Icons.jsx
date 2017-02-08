bdb0a1cd8b4ad70a202ae38af29d62907a7fe7fe
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2017-02-08 22:39:35 +0000

    Link to CV

--- src/common/components/Icons/Icons.jsx
+++ src/common/components/Icons/Icons.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => (
   <div className="icons">
     <a className="icon" href="mailto:jonnywyatt@yahoo.co.uk"><i className="icon-envelope" /></a>
-    <a className="icon" href="http://TBC"><i className="icon-cv" /></a>
+    <a className="icon" href="/pdf/jon_wyatt_cv_2017.pdf"><i className="icon-cv" /></a>
     <a className="icon" href="http://github.com/jonnywyatt"><i className="icon-github" /></a>
     <a className="icon" href="https://uk.linkedin.com/in/jonwyatt"><i
       className="icon-linkedin" /></a>
