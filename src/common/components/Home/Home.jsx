f0a802424fd6689a6feb252d2e8f18dcd99d389f
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2017-02-08 22:40:37 +0000

    Home page content

--- src/common/components/Home/Home.jsx
+++ src/common/components/Home/Home.jsx
@@ -4,8 +4,27 @@ const Home = () => (
   <div className="view home">
     <div className="container-fluid-max">
       <div className="row">
-        <div className="col-12-sm">
-          <div>Home</div>
+        <div className="col-12">
+          <div className="margin-bottom-ml">I'm an experienced freelance front end
+            and Node.js developer with skills in:</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <p className="heading heading--3">page load performance</p>
+          <p className="heading heading--3">React / Redux</p>
+          <p className="heading heading--3">scalable CSS</p>
+          <p className="heading heading--3">Node.js</p>
+          <p className="heading heading--3">accessibility</p>
+          <p className="heading heading--3">Javascript</p>
+        </div>
+        <div className="col-6">
+          <p className="heading heading--3">progressive enhancement</p>
+          <p className="heading heading--3">automated tests</p>
+          <p className="heading heading--3">responsive</p>
+          <p className="heading heading--3">CI / CD</p>
+          <p className="heading heading--3">agile</p>
+          <p className="heading heading--3">mobile</p>
         </div>
       </div>
     </div>
