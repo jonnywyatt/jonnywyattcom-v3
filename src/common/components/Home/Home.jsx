857643f3a7a1e126a32a9508551ac44ac4c247b2
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2017-02-13 23:11:51 +0000

    Mobile styling - use inverted header always

--- src/common/components/Home/Home.jsx
+++ src/common/components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => (
     <div className="container-fluid-max">
       <div className="row">
         <div className="col-8">
-          <p><strong>I have a very strong track record of rapidly delivering quality
+          <p className="margin-top-none"><strong>I have a very strong track record of rapidly delivering quality
             software.</strong></p>
           <p>The core of my skillset is front end and I’ve added over 2 years’ solid experience in
             Node.js and React. I have a good handle on the array of developments in the Javascript
@@ -14,15 +14,15 @@ const Home = () => (
             web performance and know how to measure and achieve it.</p>
           <p>I understand every angle of a successful agile team including cross-disciplinary
             collaboration, user-centred testing and design, lean UX, early & continuous delivery,
-            TDD, test automation, pairing and how to funnel business requirements through user
+            TDD, test automation and how to funnel business requirements through user
             stories.</p>
           <div className="margin-bottom-ml">I have skills in:</div>
           <div className="row">
             <div className="col-6">
-              <p className="heading heading--4">page load performance</p>
               <p className="heading heading--4">React / Redux</p>
               <p className="heading heading--4">scalable CSS</p>
               <p className="heading heading--4">Node.js</p>
+              <p className="heading heading--4">page load performance</p>
               <p className="heading heading--4">accessibility</p>
               <p className="heading heading--4">Javascript</p>
               <p className="heading heading--4">microservices</p>
