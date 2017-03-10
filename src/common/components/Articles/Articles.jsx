66acafca1e8406f2eb5624517bda18c7b1e9f250
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2017-03-10 23:32:41 +0000

    Remove nested grid

--- src/common/components/Articles/Articles.jsx
+++ src/common/components/Articles/Articles.jsx
@@ -1,33 +1,27 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import Article from '../../containers/Article';
 
 const Articles = props => (
   <div className="view articles">
-    <div className="container-fluid-max">
-      <div className="row">
-        <div className="col-12-sm">
-          {
-            (
-              props.activeRouteName === 'articles'
-              && props.articles
-              && props.articles.map((article, idx) => (
-                <div className="margin-bottom-sm" key={idx}>
-                  <h2 className="heading heading--3 margin-right-md"><a
-                    href={`/articles/${article.slug}`}
-                    onClick={props.startRouteChange}>{article.title}</a></h2>
-                  <span className="text-sm text-discreet text-light ">{article.createdDateDisplay}</span>
-                </div>
-              ))
-            )
-          }
-          {
-            (
-              props.activeRouteName === 'article' && <Article />
-            )
-          }
-        </div>
-      </div>
-    </div>
+    {
+      (
+        props.activeRouteName === 'articles'
+        && props.articles
+        && props.articles.map((article, idx) => (
+          <div className="margin-bottom-sm" key={idx}>
+            <h2 className="heading heading--3 margin-right-md"><a
+              href={`/articles/${article.slug}`}
+              onClick={props.startRouteChange}>{article.title}</a></h2>
+            <span className="text-sm text-discreet text-light ">{article.createdDateDisplay}</span>
+          </div>
+        ))
+      )
+    }
+    {
+      (
+        props.activeRouteName === 'article' && <Article />
+      )
+    }
   </div>);
 
 Articles.propTypes = {
