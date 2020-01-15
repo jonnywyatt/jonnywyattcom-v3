d0d8dd3158b1f401170430312317dd1bc6eb5790
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2020-01-15 08:41:28 +0000

    Update CV & home page description

--- src/common/components/Home/Home.jsx
+++ src/common/components/Home/Home.jsx
@@ -12,18 +12,18 @@ class Home extends React.Component {
       <div className="view home">
         <MobileAnim />
         <p className="margin-top-none"><a className="icon-text icon-text--sm" href="/articles/front-end-at-financial-times" onClick={this.props.startRouteChange}><i className="icon-chevron-right-blue padding-right-xs" />New article - <span className="text-bold">'Front end development at the FT'</span></a></p>
-        <p>The core of my skillset is front end and I’ve added over 3 years’ solid experience in
-          Node.js and React. I have a good handle on the array of developments in the Javascript
+        <p>The core of my skillset is front end and I’ve added over 5 years’ solid experience in
+          Node.js. I have a good handle on the array of developments in the Javascript
           space, plus the experience to know which of them to benefit from. I’m passionate about
           web performance and know how to measure and achieve it.</p>
-        <p>I understand every angle of a successful agile team including cross-disciplinary
+        <p>I understand every angle of a successful agile team including DevOps, cross-disciplinary
           collaboration, user-centred testing and design, lean UX, early & continuous delivery,
           TDD, test automation and how to funnel business requirements through user
           stories.</p>
         <div className="margin-bottom-ml">I have skills in:</div>
         <div className="columns columns--2">
-          <p className="heading heading--4">React / Redux</p>
-          <p className="heading heading--4">scalable CSS</p>
+          <p className="heading heading--4">React</p>
+          <p className="heading heading--4">scalable CSS including CSS-in-JS</p>
           <p className="heading heading--4">Node.js</p>
           <p className="heading heading--4">page load performance</p>
           <p className="heading heading--4">accessibility</p>
