cd5042c24f8d01ac235aa0383a24cb10b489925e
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2020-03-01 16:33:33 +0000

    Import proptypes from module

--- src/common/components/App.jsx
+++ src/common/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Home from '../containers/Home';
 import Contact from './Contact/Contact';
 import Header from '../components/Header/Header';
